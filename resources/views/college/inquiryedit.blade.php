ada9c8855b129055979abeaa1073d67462e88370
Sabin Gopali <sabingopali22@gmail.com>
2025-08-15 09:55:51 +0545

    hello

--- resources/views/college/inquiryedit.blade.php
+++ resources/views/college/inquiryedit.blade.php
@@ -1,14 +1,18 @@
 @extends('layouts.college')
 @section('content')
 <div class="container mt-5">
-    <h2 class="text-center">Update Inquiry</h2>
+    <h2 class="text-center">Reply to Inquiry</h2>
     <form action="{{ route('inquiry.update', $inquiry->id) }}" method="POST">
     @csrf
     <div class="mb-3">
-        <label for="message" class="form-label">Message</label>
-        <textarea style="height: 150px;" class="form-control" id="message" name="message" required>{{ $inquiry->message }}</textarea>
+        <label for="message" class="form-label">Student Message</label>
+        <textarea style="height: 150px;" class="form-control" id="message" name="message" readonly>{{ $inquiry->message }}</textarea>
     </div>
-    <button type="submit" class="btn btn-primary">Update</button>
+    <div class="mb-3">
+        <label for="reply" class="form-label">Your Reply</label>
+        <textarea style="height: 150px;" class="form-control" id="reply" name="reply">{{ $inquiry->reply }}</textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Save Reply</button>
 </form>
 
 </div>
